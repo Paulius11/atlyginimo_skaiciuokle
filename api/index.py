1d597450f3032cfab7e378d21812cf3737828d45
Paulius11 <paulius.gaizauskas@gmail.com>
2023-03-05 14:10:46 +0200

    Skaiciuokle v1

--- api/index.py
+++ api/index.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
+from logika import atlyginimo_skaiciuokle
 
 app = Flask(__name__)
 
+
+@app.route("/netto", methods=['GET', 'POST'])
+def neto():
+    if request.method == 'POST':
+        atlyginimas_ant_popieriaus = request.form['atlyginimas']
+        atskaiciuota = atlyginimo_skaiciuokle(int(atlyginimas_ant_popieriaus))
+        return render_template('rezultatas.html', rezultatas=atskaiciuota)
+
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('index.html')
---
+++ api/logika.py
@@ -0,0 +1,67 @@
+from dataclasses import dataclass
+
+npd_kai_nevirsija_1926 = lambda atlyginimas: 625 - 0.42 * (atlyginimas - 840)
+npd_kai_virsija_1926 = lambda atlyginimas: 400 - 0.18 * (atlyginimas - 642)
+
+
+def vsd(p_atlyginimas):
+    """
+     VSD - 12.52%
+    """
+    return p_atlyginimas / 100 * 12.52
+
+
+def psd(p_atlyginimas):
+    """
+     VSD - 12.52%
+    """
+    return p_atlyginimas / 100 * 6.98
+
+
+def gpm(p_atlyginimas):
+    """
+     GPM - 20.00%
+    :param p_atlyginimas: atlyginimas po npd
+    """
+    return p_atlyginimas / 100 * 20
+
+
+def npd(atlyginimas_burto: float) -> float:
+    """
+    NPD pagal 2023-01-01 https://www.vmi.lt/evmi/npd-pnpd-taikymas-20-str.-1
+    :param atlyginimas_burto: atlyginimas ant popieriaus
+    :return: npd
+    """
+    npd = 625
+    if atlyginimas_burto < 840:
+        return npd
+    elif 840 <= atlyginimas_burto <= 1926:
+        npd = npd_kai_nevirsija_1926(atlyginimas_burto)
+    elif atlyginimas_burto > 1926:
+        npd = npd_kai_virsija_1926(atlyginimas_burto)
+    return npd if npd > 0 else 0
+
+
+def atlyginimo_skaiciuokle(atlyginimas):
+    """
+    Pagrindinis atlyginimo apskaičiavimas
+    :param atlyginimas:
+    :return:
+    """
+    npd_ = npd(atlyginimas)
+    gpm_ = gpm(atlyginimas - npd_)
+    psd_ = psd(atlyginimas)
+    vsd_ = vsd(atlyginimas)
+    atlyginimas_i_rankas = atlyginimas - (gpm_ + psd_ + vsd_)
+
+    @dataclass(frozen=True)
+    class AtlyginimoDuomenys:
+        npd: float
+        gpm: float
+        psd: float
+        vsd: float
+        atlyginimas_i_rankas: float
+
+    data = AtlyginimoDuomenys(npd_, gpm_, psd_, vsd_, atlyginimas_i_rankas)
+    print(f"{data}")
+    return data
